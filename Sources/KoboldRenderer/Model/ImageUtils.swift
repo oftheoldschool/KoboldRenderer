dfbf308962f9ae13c2fce9f51e5bd38db77f0096
Andy Stanton <andystanton@users.noreply.github.com>
2025-07-17 22:24:41 +0100

    Switch out UIImage with CGImage

--- Sources/KoboldRenderer/Model/ImageUtils.swift
+++ Sources/KoboldRenderer/Model/ImageUtils.swift
@@ -1,5 +1,6 @@
 import Foundation
-import UIKit
+import CoreGraphics
+import ImageIO
 import UniformTypeIdentifiers
 
 public struct KImageData {
@@ -8,54 +9,74 @@ public struct KImageData {
     public let data: [UInt8]
 }
 
-extension UIImage {
+extension CGImage {
     public func pixelsRGBA() -> (Int, Int, [UInt8]) {
-        guard let cgImage = self.cgImage else {
-            return (0, 0, [])
-        }
-        let bitsPerComponent = cgImage.bitsPerComponent
-        let bitsPerPixel = cgImage.bitsPerPixel
-        let componentCount = bitsPerPixel / bitsPerComponent
-        let bytesPerRow = cgImage.bytesPerRow
-        let dataSize = cgImage.width * cgImage.height * componentCount
-        var pixelData = [UInt8](repeating: 0, count: Int(dataSize))
-        let bitmapInfo = cgImage.bitmapInfo
-        let colorSpace = cgImage.colorSpace ?? CGColorSpaceCreateDeviceRGB()
-        
-        let _ = switch cgImage.alphaInfo {
-        case .premultipliedLast: "premultipliedLast"
-        case .alphaOnly: "alphaOnly"
-        case .first: "first"
-        case .last: "last"
-        case .none: "none"
-        case .noneSkipFirst: "noneSkipFirst"
-        case .noneSkipLast: "noneSkipLast"
-        case .premultipliedFirst: "premultipliedFirst"
-        default: "unknown"
-        }
-        
-        let context = CGContext(
+        let width = self.width
+        let height = self.height
+        let componentCount = 4 
+        let bytesPerRow = width * componentCount
+        let dataSize = width * height * componentCount
+
+        var pixelData = [UInt8](repeating: 0, count: dataSize)
+
+        let colorSpace = CGColorSpaceCreateDeviceRGB()
+        let bitmapInfo = CGBitmapInfo(rawValue: CGImageAlphaInfo.premultipliedLast.rawValue)
+
+        guard let context = CGContext(
             data: &pixelData,
-            width: Int(cgImage.width),
-            height: Int(cgImage.height),
-            bitsPerComponent: bitsPerComponent,
+            width: width,
+            height: height,
+            bitsPerComponent: 8,
             bytesPerRow: bytesPerRow,
             space: colorSpace,
-            bitmapInfo: bitmapInfo.rawValue)
-        context?.draw(
-            cgImage,
-            in: CGRect(x: 0, y: 0, width: size.width, height: size.height))
-        
-        return (cgImage.width, cgImage.height, pixelData)
+            bitmapInfo: bitmapInfo.rawValue
+        ) else {
+            return (0, 0, [])
+        }
+
+        context.draw(self, in: CGRect(x: 0, y: 0, width: width, height: height))
+
+        return (width, height, pixelData)
     }
 }
 
-public func loadImageData(data: Data) -> KImageData {
-    let uiImage = UIImage(data: data)!
-    let (width, height, image) = uiImage.pixelsRGBA()
-    
+public func loadImageData(data: Data) -> KImageData? {
+    guard let dataProvider = CGDataProvider(data: data as CFData),
+          let cgImage = CGImage(
+            jpegDataProviderSource: dataProvider,
+            decode: nil,
+            shouldInterpolate: true,
+            intent: .defaultIntent
+          ) ?? CGImage(
+            pngDataProviderSource: dataProvider,
+            decode: nil,
+            shouldInterpolate: true,
+            intent: .defaultIntent
+          ) else {
+        // Fallback to ImageIO for other formats
+        return loadImageDataWithImageIO(data: data)
+    }
+
+    let (width, height, pixelData) = cgImage.pixelsRGBA()
+
     return KImageData(
         width: width,
         height: height,
-        data: image)
+        data: pixelData
+    )
+}
+
+private func loadImageDataWithImageIO(data: Data) -> KImageData? {
+    guard let imageSource = CGImageSourceCreateWithData(data as CFData, nil),
+          let cgImage = CGImageSourceCreateImageAtIndex(imageSource, 0, nil) else {
+        return nil
+    }
+
+    let (width, height, pixelData) = cgImage.pixelsRGBA()
+
+    return KImageData(
+        width: width,
+        height: height,
+        data: pixelData
+    )
 }
